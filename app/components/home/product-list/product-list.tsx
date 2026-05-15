3f2992056d7a4e9def92f821a278e5b3b8b0ed37
AnhLDP <leducphananh@gmail.com>
2026-05-15 17:01:55 +0700

    feat: add phoneVariants constant and update ProductList to include variant selection

--- app/components/home/product-list/product-list.tsx
+++ app/components/home/product-list/product-list.tsx
@@ -1,12 +1,42 @@
+'use client';
+import { phoneVariants } from '@/app/constants/phone-variant.constant';
 import { productList } from '@/app/constants/product-list.constant';
+import clsx from 'clsx';
+import Link from 'next/link';
+import { useState } from 'react';
 import ProductItem from './product-item';
 
 const ProductList = () => {
+  const [selectedVariant, setSelectedVariant] = useState(phoneVariants[0]);
+
   return (
-    <div className="mx-auto grid w-full max-w-360 grid-cols-2 gap-5 p-5 md:grid-cols-4">
-      {productList.map(product => (
-        <ProductItem key={product.id} product={product} />
-      ))}
+    <div className="mx-auto w-full max-w-360">
+      <ul className="no-scrollbar mt-2 flex items-center justify-start gap-6 overflow-auto px-5 md:justify-center">
+        {phoneVariants.map(variant => (
+          <li key={variant.id}>
+            <Link
+              href="#"
+              onClick={() => setSelectedVariant(variant)}
+              className="inline-flex items-center justify-center py-4">
+              <span
+                className={clsx(
+                  "relative text-[16px] font-bold whitespace-nowrap after:absolute after:-bottom-1 after:left-0 after:h-0.5 after:w-full after:bg-black after:transition-all after:content-['']",
+                  selectedVariant.id === variant.id
+                    ? 'after:scale-x-100'
+                    : 'after:scale-x-0 hover:text-[#000000b3]',
+                )}>
+                {variant.name}
+              </span>
+            </Link>
+          </li>
+        ))}
+      </ul>
+
+      <div className="grid grid-cols-2 gap-5 p-5 md:grid-cols-4">
+        {productList.map(product => (
+          <ProductItem key={product.id} product={product} />
+        ))}
+      </div>
     </div>
   );
 };
---
+++ app/constants/phone-variant.constant.ts
@@ -0,0 +1,25 @@
+export const phoneVariants: {
+  id: number;
+  name: string;
+}[] = [
+  {
+    id: 1,
+    name: 'Tất cả',
+  },
+  {
+    id: 2,
+    name: 'Galaxy S',
+  },
+  {
+    id: 3,
+    name: 'Galaxy Z',
+  },
+  {
+    id: 4,
+    name: 'Galaxy A',
+  },
+  {
+    id: 5,
+    name: 'Phụ kiện Điện thoại Galaxy',
+  },
+];
